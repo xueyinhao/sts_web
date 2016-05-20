b3f7481066450f9930656a2870ff45430e718e18
xueyinhao <xueyinhao@gmail.com>
2016-05-20 14:59:46 +0800

    测试上传

--- src/main/java/com/common/CommonUtils.java
+++ src/main/java/com/common/CommonUtils.java
@@ -1,10 +1,20 @@
 package com.common;
 
 public class CommonUtils {
-
+	static{
+		System.out.println("this is in CommonUtils static");
+	}
+	
+	public static String CONST = "this is const";
+	
+	private String str = "";
+	
+	public CommonUtils() {
+		System.out.println("this is in CommonUtils 构造函数");
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("this is test");
 	}
 
 }
